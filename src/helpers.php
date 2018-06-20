e098ded7494f29c0f6e9575f5fc8d0583330b118
Nick Goris <nick@pixeo.be>
2018-06-20 09:44:03 +0200

    add default parameter to setting helper

--- src/helpers.php
+++ src/helpers.php
@@ -4,8 +4,8 @@ if (! function_exists('setting')) {
     /**
      * @return \Pixeo\Settings\Setting
      */
-    function setting($key)
+    function setting($key, $default = null)
     {
-        return data_get(app('settings'), $key);
+        return data_get(app('settings'), $key, $default);
     }
 }
