8d859b8b221b1e4b11f6416b6e351d49f4d01564
Nick Goris <nick@pixeo.be>
2018-09-25 11:29:17 +0200

    add cache key to settings

--- config/settings.php
+++ config/settings.php
@@ -1,8 +1,13 @@
 <?php
 
 return [
-    /* 
+    /*
      * The model that is used to fetch the settings
      */
     'model' => Pixeo\Settings\Setting::class,
+
+    /**
+     * The cache key the package will use
+     */
+    'cache_key' => 'pixeo.settings',
 ];
--- src/SettingServiceProvider.php
+++ src/SettingServiceProvider.php
@@ -26,7 +26,7 @@ class SettingServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/settings.php', 'settings');
 
         $this->app->singleton('settings', function ($app) {
-            return $app['cache']->rememberForever('site.settings', function () {
+            return $app['cache']->rememberForever($app['config']->get('settings.cache_key'), function () {
                 $result = [];
                 $class = $this->app['config']->get('settings.model');
 
